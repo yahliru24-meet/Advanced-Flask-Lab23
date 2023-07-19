d71dc15856b1b5ca8738be9cb584915b5edf9048
yahliru24-meet <93874001+yahliru24-meet@users.noreply.github.com>
2023-07-19 13:41:41 +0300

    :airplane:

--- Advanced-Flask-Lab/app.py
+++ Advanced-Flask-Lab/app.py
@@ -25,7 +25,7 @@ posts = {
 
 @app.route('/')  # '/' for the default page
 def home():
-    return render_template('index.html', prof_pic=image_link, bio=user_bio, posts=posts)
+    return render_template('index.html', prof_pic=image_link, bio=user_bio, posts=posts, counter=0)
 
 
 @app.route('/about')  # '/' for the default page
